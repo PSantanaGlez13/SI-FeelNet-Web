08a1dd687fa5248c20cff315ad15ecfc18f74d66
PSantanaGlez13 <alu0101480541@ull.edu.es>
2025-01-07 18:59:57 +0000

    Models.ts updated

--- src/web/models.ts
+++ src/web/models.ts
@@ -3,7 +3,42 @@ import { showEmptyWordMessage } from "./error-messages";
 import { Loader } from "./loader";
 import { resizeElementToMax } from "./dynamic-resizer";
 
-function evaluateWord(resultChart: Chart): void {
+interface DatasetInfo {
+  label: string;
+  data: number[]
+}
+
+interface DataChart {
+  labels: string[];
+  datasets: Array<DatasetInfo>;
+}
+
+function formatData(labels: string[], data: number[]): DataChart {
+  return {
+    labels,
+    datasets: [{
+      label: "Result of the query",
+      data
+    }]
+  }
+}
+
+function getChart(canvas: HTMLCanvasElement, data: DataChart | null = null): Chart<"bar"> {
+  const DEFAULT_DATA: DataChart = {
+    labels: ["Sadness", "Joy", "Love", "Anger", "Fear", "Surprise"],
+    datasets: [{
+      label: "Result of the query",
+      data: [0, 0, 0, 0, 0, 0]
+    }]
+  }
+  if (data === null) data = DEFAULT_DATA;
+  return new Chart(canvas, {
+    type: "bar",
+    data
+  });
+}
+
+function evaluateWord(resultChart: Chart, canvas: HTMLCanvasElement): void {
   const WORD_TO_EVAL: HTMLInputElement = document.getElementById("word")! as HTMLInputElement;
   const WORD_CONTENT: string = WORD_TO_EVAL.value;
   if (WORD_CONTENT.match(/^\s*$/)) {
@@ -14,17 +49,24 @@ function evaluateWord(resultChart: Chart): void {
   const MODEL_TEXT: string = MODEL_TO_EVAL.value;
   const QUERY_URL: string = `model=${MODEL_TEXT}&&word=${WORD_CONTENT}`;
   console.log("Trying to fulfill the request: ", QUERY_URL);
+  resultChart.destroy();
+  const LOADER = new Loader("Scraping and evaluating with the model", "result-chart");
+  LOADER.startAnimation();
   fetch(QUERY_URL)
     .then(async (response) => {
       // Do smth with the result.
-      resultChart.destroy();
-      const LOADER = new Loader("Im doing stuff", "result-chart");
-      LOADER.startAnimation();
-      const TEXT = await response.text();
-      console.log("Request fulfilled!", TEXT);
+      const JSON = await response.json();
+      LOADER.stopAnimation();
+      const LABELS = JSON.labels;
+      const COUNTS = JSON.counts;
+      resultChart = getChart(canvas, formatData(LABELS, COUNTS));
+      console.log("Request fulfilled!", JSON);
     })
     .catch(() => {
       // Do smth with the error.
+      LOADER.stopAnimation();
+      resultChart = getChart(canvas);
+      resultChart.data.datasets[0].data = [0, 0, 0, 0, 0, 0];
       console.log("Could not fulfill the request!");
     })
 }
@@ -33,19 +75,10 @@ function evaluateWord(resultChart: Chart): void {
 function main(): void {
   const BUTTON: HTMLButtonElement = document.getElementById("evaluate")! as HTMLButtonElement;
   const CANVAS: HTMLCanvasElement = document.getElementById("result-chart")! as HTMLCanvasElement;
-  let resultChart: Chart = new Chart(CANVAS, {
-    type: "bar",
-    data: {
-      labels: ["Sadness", "Joy", "Love", "Anger", "Fear", "Surprise"],
-      datasets: [{
-        label: "Result of the query",
-        data: [5, 3, 2, 4, 0, 1]
-      }]
-    }
-  });
+  let resultChart: Chart = getChart(CANVAS);
   resizeElementToMax(CANVAS);
   window.addEventListener("resize", () => resizeElementToMax(CANVAS));
-  BUTTON.addEventListener("click", () => evaluateWord(resultChart));
+  BUTTON.addEventListener("click", () => evaluateWord(resultChart, CANVAS));
   // Get button
   // Add onClick event -> Function for the event callback
   // Construct query with the values of the "word" and "models"
